61d49a97d927c0b53e8fdce0f49404f4befa11e0
MiroslavskyCoder <programmer_js@vk.com>
2025-10-19 03:33:14 +0300

    Update project configuration and documentation formatting

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const packageJson = require('./package.json');
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: 'tsconfig.build.json',
+      useTsconfigDeclarationDir: true,
+      clean: true,
+    }),
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      },
+      extract: 'styles.css',
+      minimize: true,
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom', 'react-router-dom'],
+};
