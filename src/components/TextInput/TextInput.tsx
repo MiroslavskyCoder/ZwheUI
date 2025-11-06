475a5abc7031121add3a9ee035dfbe261ce2b4dd
MiroslavskyCoder <programmer_js@vk.com>
2025-11-07 01:17:24 +0300

    Fix indentation and update version to 1.0.4-6.5

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -12,8 +12,7 @@ export const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(({ c
     const isDark = theme.colors.background.startsWith('#1');
 
     const baseClasses = createStyle({
-        display: 'block',
-        width: '100%',
+        display: 'block', 
         backgroundColor: theme.colors.backgroundSecondary,
         border: `1px solid ${theme.colors.border}`,
         borderRadius: '0.375rem',
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         use: [
-          {
+          { 
             loader: 'babel-loader',
             options: {
               presets: [
